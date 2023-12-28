7e040372e09f1b3b19d7b89619edf49d3067aac1
RedTeamerGray <miriithidennis340@gmail.com>
2023-12-28 23:57:16 +0300

    code commit

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
-const express = require ('express')
-const app = express()
+// import the required packages
+const express = require('express');
+const bodyParser = require('body-parser');
 
+// initialize the express app
+const app = express();
+
+// middleware to parse the body of POST, PUT, DELETE requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// set up a simple GET request
 app.get('/', (req, res) => {
-    res.send("hello, world")
-})
+    res.send('Welcome to the Express API');
+});
+
+// set up a POST request
+app.post('/post-example', (req, res) => {
+    console.log(req.body);
+    res.send('Post request received');
+});
+
+// set up a PUT request
+app.put('/put-example/:id', (req, res) => {
+    console.log(req.params.id);
+    console.log(req.body);
+    res.send('Put request received');
+});
+
+// set up a DELETE request
+app.delete('/delete-example/:id', (req, res) => {
+    console.log(req.params.id);
+    res.send('Delete request received');
+});
+
+// start the server
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
